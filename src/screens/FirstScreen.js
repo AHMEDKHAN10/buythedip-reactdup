9e1b7d73b17b3044f172ed15ace4688db3c3aa9a
Ahmed Khan <khanmuham@deakin.edu.au>
2021-05-11 07:34:20 +0300

    settings cleaned up

--- src/screens/FirstScreen.js
+++ src/screens/FirstScreen.js
@@ -1,220 +1,114 @@
-import { useNavigation } from "@react-navigation/native";
-import React, { useState, useEffect } from "react";
+import { useNavigation } from '@react-navigation/native'
+import React, { useState, useEffect } from 'react'
 import {
-  StyleSheet,
   SafeAreaView,
   View,
   Text,
-  TextInput,
-  Dimensions,
-  Button,
-  TouchableOpacity,
-  TouchableHighlight,
-} from "react-native";
-import stocks from "../constants";
-import { Menu, ActivityIndicator, Colors } from "react-native-paper";
-
-import axios from 'axios'
-// import LoadingOverlay from 'react-loading-overlay';
-// const A = require('../constants/CompanyDeets/A.json');
-// const B = require('../constants/CompanyDeets/B.json');
-// const C = require('../constants/CompanyDeets/C.json');
-// const D = require('../constants/CompanyDeets/D.json');
-// const E = require('../constants/CompanyDeets/E.json');
-// const F = require('../constants/CompanyDeets/F.json');
-// const G = require('../constants/CompanyDeets/G.json');
-// const H = require('../constants/CompanyDeets/H.json');
-// const I = require('../constants/CompanyDeets/I.json');
-// const J = require('../constants/CompanyDeets/J.json');
-// const K = require('../constants/CompanyDeets/K.json');
-// const L = require('../constants/CompanyDeets/L.json');
-// const M = require('../constants/CompanyDeets/M.json');
-// const N = require('../constants/CompanyDeets/N.json');
-// const O = require('../constants/CompanyDeets/O.json');
-// const P = require('../constants/CompanyDeets/P.json');
-// const Q = require('../constants/CompanyDeets/Q.json');
-// const R = require('../constants/CompanyDeets/R.json');
-// const S = require('../constants/CompanyDeets/S.json');
-// const T = require('../constants/CompanyDeets/T.json');
-// const U = require('../constants/CompanyDeets/U.json');
-// const V = require('../constants/CompanyDeets/V.json');
-// const W = require('../constants/CompanyDeets/W.json');
-// const X = require('../constants/CompanyDeets/X.json');
-// const Y = require('../constants/CompanyDeets/Y.json');
-// const Z = require('../constants/CompanyDeets/Z.json');
-
-import Autocomplete from "react-native-autocomplete-input";
-
-const height = Dimensions.get("screen").height;
-
-// let companySymbol
-var p
-function FirstScreen() {
-  const [symbols, setSymbols] = useState();
-  const navigation = useNavigation();
-  const [query, setQuery] = useState("");
-  const [data, setData] = useState([]);
+  Dimensions
+} from 'react-native'
+import stocks from '../constants'
+import { Menu } from 'react-native-paper'
+import Autocomplete from 'react-native-autocomplete-input'
+const height = Dimensions.get('screen').height
+const API = 'http://127.0.0.1:3000/'
+function FirstScreen () {
+  const navigation = useNavigation()
+  const [query, setQuery] = useState('')
+  const [data, setData] = useState([])
   const [price, setPrice] = useState([])
-  const [loader, setloader] = useState(false)
-  // showLoader = () => {
-  //   setloader(true);
-  // };
-  
 
   const StockName = async (name) => {
-    
-    // let url = 'http://localhost:3000'
-    //let Name = JSON.stringify(name)
-    // console.log('uid', userid)
-    let request = JSON.stringify({
-      // userid : userid,
-      stock : name
+    const request = JSON.stringify({
+      stock: name
     })
     const options = {
-      method: "POST",
+      method: 'POST',
       headers: {
         Accept: 'application/json',
-        "Content-Type" : "application/json"
+        'Content-Type': 'application/json'
       },
       body: request
-    };
-    // console.log('uid', userid)
-    // console.log("name: "+ options.body)
-    const response = await fetch('http://127.0.0.1:3000/getStockName', options)
-    const json = await response.json();
-    console.log("json: ",json)
+    }
+    const response = await fetch(API + 'getStockName', options)
+    const json = await response.json()
     setPrice(json.price)
-    p = json.price
-    // await fetch('http://127.0.0.1:3000/getData')
-    // setloader(true)
-    // setloader(false)
   }
 
-  useEffect(()=>{
-    // console.log("p: ", p)
-    // setPrice(p)
-    //console.log("price: ", price)
-  }, [price])
-
-  function renderHeader(navigation) {
+  function renderHeader (navigation) {
     return (
-      // flex:1,
       <View
         style={{
           flex: 1,
           paddingRight: 20,
-          flexDirection: "row",
-          justifyContent: "center",
-          justifyContent: "space-between",
-          width: "100%",
-          height: 10,
+          flexDirection: 'row',
+          justifyContent: 'center',
+          width: '100%',
+          height: 10
         }}
       >
         <Text
           style={{
             padding: 10,
-            textAlign: "right",
-            width: "67%",
+            textAlign: 'right',
+            width: '67%',
             fontSize: 23,
-            height: 50,
+            height: 50
           }}
         >
-          <Text style={{ fontWeight: "800" }}>DIP</Text>
-          <Text style={{ fontWeight: "400" }}>LIST</Text>
+          <Text style={{ fontWeight: '800' }}>DIP</Text>
+          <Text style={{ fontWeight: '400' }}>LIST</Text>
         </Text>
         <Text
           style={{
             padding: 10,
             paddingTop: 15,
-            textAlign: "right",
-            width: "33%",
+            textAlign: 'right',
+            width: '33%',
             fontSize: 18,
             height: 50,
-            color: "#6fcc51",
+            color: '#6fcc51'
           }}
           onPress={() => {
-            navigation.navigate("Home", {
-            StockName: 'No specific stock selected',
-          });
+            navigation.navigate('Home', {
+              StockName: 'No specific stock selected'
+            })
           }}
         >
           Skip
         </Text>
       </View>
-    );
-  }
-  //Dimensions.get("window").width
-  function StalkHeader() {
-    return (
-      <View
-        style={{ flex: 3, padding: 10, width: Dimensions.get("window").width }}
-      >
-        <TextInput
-          placeholder="GME"
-          style={{
-            fontWeight: "400",
-            letterSpacing: 4,
-            textAlign: "center",
-            fontSize: 80,
-            height: 90,
-            color: "#c6c8c9",
-          }}
-          autoCapitalize="characters"
-        >
-          {/* <Text style={{fontWeight:'400',letterSpacing:4, textAlign:'center', fontSize:80,height: 90, color:'#c6c8c9'}}>GME</Text> */}
-        </TextInput>
-        <Text
-          style={{
-            fontWeight: "400",
-            marginTop: 4,
-            color: "#c6c8c9",
-            textAlign: "center",
-          }}
-        >
-          Last close $567.99
-        </Text>
-      </View>
-    );
+    )
   }
 
   useEffect(() => {
-    const stuff = [];
-    if (query === "") {
-      setData([]);
-    } 
-    else if (query.length === 1) {
+    const stuff = []
+    if (query === '') {
+      setData([])
+    } else if (query.length === 1) {
+      // eslint-disable-next-line array-callback-return
       stocks[query].map((element) => {
-        stuff.push(element);
-      });
-    } 
-    else if (query.length > 1) {
+        stuff.push(element)
+      })
+    } else if (query.length > 1) {
       stocks[query[0]].forEach((element) => {
         if (element.symbol.search(query) === 0) {
-          stuff.push(element);
+          stuff.push(element)
         }
-      });
+      })
     }
     if (stuff === [] || stuff.length === 1) {
-      setData([]);
+      setData([])
     } else {
-      setData(stuff);
+      setData(stuff)
     }
-  }, [query]);
+  }, [query])
 
-  // console.log("query: "+query)
   return (
-    
-    <SafeAreaView
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    >
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       {renderHeader(navigation)}
-      
-
-      <View
-        style={{ flex: 3, padding: 10, width: Dimensions.get("window").width }}
-      >
+      <View style={{ flex: 3, padding: 10, width: Dimensions.get('window').width }}>
        <Autocomplete
-          data={data}
+         data={data}
           defaultValue={query}
           placeholder="GME"
           onChangeText={(value) => setQuery(value)}
@@ -222,121 +116,71 @@ function FirstScreen() {
             <Menu.Item
               key={index}
               onPress={async () => {
-                setQuery(item.symbol);
+                setQuery(item.symbol)
                 setData([])
                 //* to send company name to next screen
-                // navigation.navigate("SetAlert")
-                // setTimeout(await StockName(item.symbol), 5000);
                 await StockName(item.symbol)
-                console.log("price: ", p)
-                if(p == 'Stock price not available'){
-                  // navigation.navigate("SetAlert", {
-                  //   otherParam: item.symbol,
-                  //   price : p
-                  // })
-                  alert(p)
-                }else{
-                  // <ActivityIndicator animating={true} color={Colors.red800} />
-                  navigation.navigate("SetAlert", {
+                console.log('price: ', price)
+                if (price === 'Stock price not available') {
+                  alert(price)
+                } else {
+                  navigation.navigate('SetAlert', {
                     otherParam: item.symbol,
-                    price : '$'+p
+                    price: '$' + price
                   })
                 }
-                
               }}
               title={
-                <Text style={{ fontWeight: "bold" }}>
-                  {item.symbol}{" "}
+                <Text style={{ fontWeight: 'bold' }}>
+                  {item.symbol}{' '}
                   <Text style={{ fontSize: 10 }}>{item.name}</Text>
                 </Text>
               }
             />
           )}
           style={{
-            fontWeight: "400",
+            fontWeight: '400',
             letterSpacing: 4,
-            textAlign: "center",
+            textAlign: 'center',
             fontSize: 80,
-            height: 90,
+            height: 90
           }}
-          listStyle={{maxHeight:0.4*height, borderColor:"transparent"}}
-          flatListProps={{showsVerticalScrollIndicator:false}}
-          inputContainerStyle={{ borderColor: "transparent"}}
-          keyExtractor={(item,index)=>{
-            return item.symbol;
+          listStyle={{ maxHeight: 0.4 * height, borderColor: 'transparent' }}
+          flatListProps={{ showsVerticalScrollIndicator: false }}
+          inputContainerStyle={{ borderColor: 'transparent' }}
+          keyExtractor={(item, index) => {
+            return item.symbol
           }}
 
           autoCapitalize="characters"
         />
-        { p = 'Stock price not available' 
-        ? <Text
+        { price === 'Stock price not available'
+          ? <Text
             placeholder='abc'
             style={{
-              fontWeight: "400",
+              fontWeight: '400',
               marginTop: 4,
-              color: "#c6c8c9",
-              textAlign: "center",
+              color: '#c6c8c9',
+              textAlign: 'center'
             }}
           >
             last closed at $567.99
           </Text>
-        : <Text
+          : <Text
             placeholder='abc'
             style={{
-              fontWeight: "400",
+              fontWeight: '400',
               marginTop: 4,
-              color: "#000",
-              textAlign: "center",
+              color: '#000',
+              textAlign: 'center'
             }}
           >
-          las closed at ${price}
-          </Text>}
+          last closed at ${price}
+          </Text>
+        }
       </View>
-    </SafeAreaView> 
-  );
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#F5FCFF",
-    flex: 1,
-    padding: 16,
-    marginTop: 40,
-  },
-  autocompleteContainer: {
-    backgroundColor: "#ffffff",
-    borderWidth: 0,
-  },
-  descriptionContainer: {
-    flex: 1,
-    justifyContent: "center",
-  },
-  itemText: {
-    fontSize: 15,
-    paddingTop: 5,
-    paddingBottom: 5,
-    margin: 2,
-  },
-  infoText: {
-    textAlign: "center",
-    fontSize: 16,
-  },
-  ButtonAddStock:{
-    width:'85%',
-    backgroundColor:'#2b3033',
-    height: 58,
-    borderRadius:40,
-    justifyContent: 'center',
-    borderWidth: 1,
-    borderColor: '#2b3033'
-    },
-ButtonAddStockText:{
-    color:'#ffe6d2',
-    fontSize: 20,
-    textAlign:'center',
-    paddingLeft : 10,
-    paddingRight:10
-}
-});
-
-export default FirstScreen;
+export default FirstScreen
--- src/screens/settings.js
+++ src/screens/settings.js
@@ -1,131 +1,94 @@
-import React, {useEffect, useState} from 'react'
-import { useNavigation } from "@react-navigation/native";
-import { 
-    StyleSheet, 
-    SafeAreaView, 
-    View, Text, 
-    TextInput, 
-    Dimensions, 
-    Button, 
-    ScrollView,
-    KeyboardAvoidingView,
-    TouchableOpacity, 
-    Platform, 
-    TouchableWithoutFeedback,
-    TouchableHighlight, 
-    Keyboard,
-    // Switch
+import React, { useEffect, useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import {
+  StyleSheet,
+  SafeAreaView,
+  View, Text,
+  Dimensions,
+  TouchableHighlight
 } from 'react-native'
-import * as LocalAuthentication from 'expo-local-authentication'
-import * as FaceDetector from 'expo-face-detector'
-import { Camera } from 'expo-camera';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons'
-import KeyboardListener from 'react-native-keyboard-listener'
+import { Camera } from 'expo-camera'
 import { AntDesign } from '@expo/vector-icons'
-import { useFonts } from 'expo-font'
 import Switch from 'react-native-switch-pro'
 
-function renderHeader(navigation){
-  return(
-    <View style={{flex:1, flexDirection: 'row', justifyContent:'space-between', width:'100%', height: 10}}>
-      {/* <FontAwesomeIcon icon={ faLongArrowAltLeft } size={ 30 } style={{ marginTop: 5, paddingLeft:'15%'}} /> */}
-      <AntDesign name="arrowleft" size={24} color="black" 
-        onPress={()=> {
-            navigation.navigate('Home')
-          }
-        } 
-        style={{ paddingTop:10, paddingLeft:35, width: '40%'}}/>          
-      <Text style={{padding: 10, textAlign:'left', width: '60%', fontSize:20}}>
-        <Text style={{fontWeight:'400'}}>Settings</Text>
+function renderHeader (navigation) {
+  return (
+    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', width: '100%', height: 10 }}>
+      <AntDesign name="arrowleft" size={24} color="black"
+        onPress={ () => {
+          navigation.navigate('Home')
+        }}
+        style={{ paddingTop: 10, paddingLeft: 35, width: '40%' }}/>
+      <Text style={{ padding: 10, textAlign: 'left', width: '60%', fontSize: 20 }}>
+        <Text style={{ fontWeight: '400' }}>Settings</Text>
       </Text>
     </View>
   )
 }
 
+function Content2 () {
+  const [isEnabled, setIsEnabled] = useState(false)
+  // eslint-disable-next-line no-unused-vars
+  const [hasPermission, setHasPermission] = useState(null)
+  // const [faces, setFaces] = useState([])
 
-
-function Content2(){
-  const [isEnabled, setIsEnabled] = useState(false);
-  const [hasPermission, setHasPermission] = useState(null);
-  const [faces, setFaces] = useState([])
-
-  const faceDetected = ({faces}) => {
-    setFaces({faces})
-    console.log('faces: ' + {faces})
-  }
+  // const faceDetected = ({ faces }) => {
+  //   setFaces({ faces })
+  //   console.log('faces: ' + { faces })
+  // }
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestPermissionsAsync()
       setHasPermission(status === 'granted')
     })()
   }, [])
-  
-  const faceDetector = () => {
-    console.log('in face detector')
-    return(
-        <Camera
-        style={{ flex: 1 }}
-        type='front'
-        // ... other props
-        onFacesDetected={faceDetected}
-        faceDetectorSettings={{
-          mode: FaceDetector.Constants.Mode.fast,
-          detectLandmarks: FaceDetector.Constants.Landmarks.none,
-          runClassifications: FaceDetector.Constants.Classifications.none,
-          minDetectionInterval: 100,
-          tracking: true,
-        }}
-      >
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: 'transparent',
-            flexDirection: 'row',
-          }}>
-          <Text style= {{top:200}}> is {faces} </Text>
-        </View>
-      </Camera>
-      
-      )
-  }
+
+  // const faceDetector = () => {
+  //   console.log('in face detector')
+  //   return (
+  //       <Camera
+  //       style={{ flex: 1 }}
+  //       type='front'
+  //       // ... other props
+  //       onFacesDetected={faceDetected}
+  //       faceDetectorSettings={{
+  //         mode: FaceDetector.Constants.Mode.fast,
+  //         detectLandmarks: FaceDetector.Constants.Landmarks.none,
+  //         runClassifications: FaceDetector.Constants.Classifications.none,
+  //         minDetectionInterval: 100,
+  //         tracking: true
+  //       }}
+  //     >
+  //       <View
+  //         style={{
+  //           flex: 1,
+  //           backgroundColor: 'transparent',
+  //           flexDirection: 'row'
+  //         }}>
+  //         <Text style= {{ top: 200 }}> is {faces} </Text>
+  //       </View>
+  //     </Camera>
+  //   )
+  // }
 
   const toggleSwitch = async () => {
-    setIsEnabled(previousState => !previousState);
-    console.log("toggle switch")
-    console.log("isenabled: " + isEnabled)
-    if(!isEnabled){
+    setIsEnabled(previousState => !previousState)
+    console.log('toggle switch')
+    console.log('isenabled: ' + isEnabled)
+    if (!isEnabled) {
       console.log('enabled')
       // faceDetector()
-    }else{
+    } else {
       console.log('not enabled')
     }
-    // if(LocalAuthentication.hasHardwareAsync()){
-    //   console.log('has auth')
-    //   // let types = LocalAuthentication.supportedAuthenticationTypesAsync()
-    //   // console.log("Types: " + JSON.stringify(types))
-    //   // if (hasPermission !== true) {
-    //   //   return <Text>No access to camera</Text>
-    //   // }
-    //   faceDetector()
-    //   // console.log("type: ", LocalAuthentication.supportedAuthenticationTypesAsync())
-    //   // if(LocalAuthentication.supportedAuthenticationTypesAsync()){
-    //   //   console.log('facial recognition')
-    //   // }
-    //   // return LocalAuthentication.supportedAuthenticationTypesAsync()
-    // }
-    // console.log("Auth: ",LocalAuthentication.hasHardwareAsync())
-    
   }
 
-  return(
-    <View style={{flex:8, width:(Dimensions.get('window').width - (0.1*(Dimensions.get('window').width))), height:50}}>
-      {/* <Ionicons name="settings-outline" size={24} color="black" style={{padding: 10}}/> */}
-      <View  style={{marginTop:10,flexDirection:'row' ,width: '100%', height: 60, marginLeft:'0%' ,borderBottomWidth :1,borderBottomColor: '#e2e3e4', alignItems:'left'}}>
-        <View style={{width:'80%'}}>
-          <Text style={{fontSize:17}}>Face ID</Text>
+  return (
+    <View style={{ flex: 8, width: (Dimensions.get('window').width - (0.1 * (Dimensions.get('window').width))), height: 50 }}>
+      <View style={{ marginTop: 10, flexDirection: 'row', width: '100%', height: 60, marginLeft: '0%', borderBottomWidth: 1, borderBottomColor: '#e2e3e4', alignItems: 'left' }}>
+        <View style={{ width: '80%' }}>
+          <Text style={{ fontSize: 17 }}>Face ID</Text>
         </View>
-        
         <Switch
           width= {57}
           height={30}
@@ -133,45 +96,41 @@ function Content2(){
           backgroundInactive='#fff'
           value = {isEnabled}
           style={{
-            borderColor:'#000',
+            borderColor: '#000',
             borderWidth: 0.5,
             padding: 2
           }}
           circleStyle={{
-            width:25,
-            height:25,
+            width: 25,
+            height: 25,
             borderWidth: 0.5,
-            borderColor:'#000',
+            borderColor: '#000'
           }}
           onSyncPress = {async () => {
             await toggleSwitch()
-            
-            // console.log("isenabled: " + isEnabled)
-            //faceDetection()
           }}
         />
-      
       </View>
-      <View  style={{marginTop:40,flexDirection:'row' ,width: '100%', height: 60, marginLeft:'0%' ,borderBottomWidth :1,borderBottomColor: '#e2e3e4', alignItems:'left'}}>
-        <View style={{width:'80%'}}>
-          <Text style={{fontSize:17}}>Manage Notifications</Text>
+      <View style={{ marginTop: 40, flexDirection: 'row', width: '100%', height: 60, marginLeft: '0%', borderBottomWidth: 1, borderBottomColor: '#e2e3e4', alignItems: 'left' }}>
+        <View style={{ width: '80%' }}>
+          <Text style={{ fontSize: 17 }}>Manage Notifications</Text>
         </View>
-        <AntDesign name="right" size={24} color="black" style={{width: '20%', justifyContent: 'center', marginLeft:'10%'}}/> 
+        <AntDesign name="right" size={24} color="black" style={{ width: '20%', justifyContent: 'center', marginLeft: '10%' }}/>
       </View>
-      <View  style={{marginTop:40,flexDirection:'row' ,width: '100%', height: 60, marginLeft:'0%' ,borderBottomWidth :1,borderBottomColor: '#e2e3e4', alignItems:'left'}}>
-        <View style={{width:'80%'}}>
-          <Text style={{fontSize:17}}>Feature Request & Report Bugs</Text>
+      <View style={{ marginTop: 40, flexDirection: 'row', width: '100%', height: 60, marginLeft: '0%', borderBottomWidth: 1, borderBottomColor: '#e2e3e4', alignItems: 'left' }}>
+        <View style={{ width: '80%' }}>
+          <Text style={{ fontSize: 17 }}>Feature Request & Report Bugs</Text>
         </View>
-        <AntDesign name="right" size={24} color="black" style={{width: '20%', justifyContent: 'center', marginLeft:'10%'}}/> 
+        <AntDesign name="right" size={24} color="black" style={{ width: '20%', justifyContent: 'center', marginLeft: '10%' }}/>
       </View>
-      <View  style={{marginTop:40, flexDirection:'row' ,width: '100%', height: 60, marginLeft:'0%' ,borderBottomWidth :1,borderBottomColor: '#e2e3e4', alignItems:'left'}}>
-        <View style={{width:'80%'}}>
-          <Text style={{fontSize:17}}>Restore subscription</Text>
+      <View style={{ marginTop: 40, flexDirection: 'row', width: '100%', height: 60, marginLeft: '0%', borderBottomWidth: 1, borderBottomColor: '#e2e3e4', alignItems: 'left' }}>
+        <View style={{ width: '80%' }}>
+          <Text style={{ fontSize: 17 }}>Restore subscription</Text>
         </View>
-        <AntDesign name="right" size={24} color="black" style={{width: '20%', justifyContent: 'center', marginLeft:'10%'}}/> 
+        <AntDesign name="right" size={24} color="black" style={{ width: '20%', justifyContent: 'center', marginLeft: '10%' }}/>
       </View>
-      <View style={{marginTop:40, flexDirection:'row' ,width: '100%', height: 60, marginLeft:'0%' , justifyContent:'center'}}>
-        <TouchableHighlight  style={styles.ButtonAddStock} onPress={()=> navigation.navigate("FirstScreen")} underlayColor='#fff'>
+      <View style={{ marginTop: 40, flexDirection: 'row', width: '100%', height: 60, marginLeft: '0%', justifyContent: 'center' }}>
+        <TouchableHighlight style={styles.ButtonAddStock} onPress={() => console.log('button tapped')} underlayColor= '#fff'>
           <Text style={styles.ButtonAddStockText}>Share with friends</Text>
         </TouchableHighlight>
       </View>
@@ -179,11 +138,10 @@ function Content2(){
   )
 }
 
-function settings() {
+function settings () {
   const navigation = useNavigation()
   return (
-    <SafeAreaView style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-    
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
     {renderHeader(navigation)}
     {Content2()}
     </SafeAreaView>
@@ -193,20 +151,20 @@ function settings() {
 export default settings
 
 const styles = StyleSheet.create({
-  ButtonAddStock:{
-    width:'85%',
-    backgroundColor:'#2b3033',
+  ButtonAddStock: {
+    width: '85%',
+    backgroundColor: '#2b3033',
     height: 58,
-    borderRadius:40,
+    borderRadius: 40,
     justifyContent: 'center',
     borderWidth: 1,
     borderColor: '#2b3033'
-    },
-  ButtonAddStockText:{
-    color:'#ffe6d2',
+  },
+  ButtonAddStockText: {
+    color: '#ffe6d2',
     fontSize: 20,
-    textAlign:'center',
-    paddingLeft : 10,
-    paddingRight:10
+    textAlign: 'center',
+    paddingLeft: 10,
+    paddingRight: 10
   }
 })
