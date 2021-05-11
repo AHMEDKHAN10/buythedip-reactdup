9a35b34e173e5fb44200bc85c229ca1addfbdaca
Ahmed Khan <khanmuham@deakin.edu.au>
2021-05-11 08:35:15 +0300

    environment variables implemented in home code, cleaned up

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react'
 import { StyleSheet, SafeAreaView, View, Text, Dimensions, TouchableOpacity, TouchableHighlight } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
