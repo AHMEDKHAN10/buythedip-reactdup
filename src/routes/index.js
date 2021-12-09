f4f7f56f5737fd3aba1bcf73c42db7d2015c6d6d
Ahmed Khan <khanmuham@deakin.edu.au>
2021-12-09 20:45:10 +0500

    code cleanup

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import 'react-native-gesture-handler'
 import Home from '../screens/Home'
@@ -13,14 +12,13 @@ import CommuinityGroup from '../screens/reuseable/commuinityGroups'
 import firebaseuser from '../firebase/firebaseconfig'
 import configg from '../../config'
 import { faClosedCaptioning } from '@fortawesome/free-solid-svg-icons'
-import { EventRegister } from 'react-native-event-listeners'
-import SplashScreen from '../screens/SplashScreen'
+// import { EventRegister } from 'react-native-event-listeners'
 
 const Stack = createStackNavigator()
 
 export default function Routes () {
   const [route, setroute] = useState(faClosedCaptioning)
-  const [darkMode, setDarkMode] = useState(false)
+  // const [darkMode, setDarkMode] = useState(false)
   useEffect(() => {
     async function check () {
       const userid = await firebaseuser()
@@ -44,16 +42,16 @@ export default function Routes () {
       setroute(false)
     }
     check()
-    const today = new Date()
-    const time = today.getHours() + ':' + today.getMinutes()
+    // const today = new Date()
+    // const time = today.getHours() + ':' + today.getMinutes()
     // console.log('isSubscribed: ' + isSubscribed)
-    if (time >= '18:25' && time <= '6:55') {
-      setDarkApp(true)
-      // EventRegister.emit('themeListener', true)
-    } else {
-      setDarkApp(false)
-      // EventRegister.emit('themeListener', false)
-    }
+    // if (time >= '18:25' && time <= '6:55') {
+    //   setDarkApp(true)
+    //   // EventRegister.emit('themeListener', true)
+    // } else {
+    //   setDarkApp(false)
+    //   // EventRegister.emit('themeListener', false)
+    // }
     // const eventListener = EventRegister.addEventListener(
     //   'themeListener',
     //   data => {
@@ -65,45 +63,42 @@ export default function Routes () {
     }
     // console.log('route: ' + route)
   }, [])
-  const MyDarkTheme = {
-    dark: true,
-    colors: {
-      primary: '#ffffff',
-      background: '#000000',
-      card: '#1C1C1D',
-      text: '#ffffff',
-      border: '#000000',
-      notification: '#EC5D29'
-    }
-  }
-  const MyTheme = {
-    dark: false,
-    colors: {
-      primary: '#2B3033',
-      background: '#ffffff',
-      card: '#ffffff',
-      text: '#2B3033',
-      border: '#FFE6D1',
-      notification: '#000000'
-    }
-  }
+  // const MyDarkTheme = {
+  //   dark: true,
+  //   colors: {
+  //     primary: '#ffffff',
+  //     background: '#000000',
+  //     card: '#1C1C1D',
+  //     text: '#ffffff',
+  //     border: '#000000',
+  //     notification: '#EC5D29'
+  //   }
+  // }
+  // const MyTheme = {
+  //   dark: false,
+  //   colors: {
+  //     primary: '#2B3033',
+  //     background: '#ffffff',
+  //     card: '#ffffff',
+  //     text: '#2B3033',
+  //     border: '#FFE6D1',
+  //     notification: '#000000'
+  //   }
+  // }
 
-  const [darkApp, setDarkApp] = useState(false)
-  const appTheme = darkApp ? MyDarkTheme : MyTheme
+  // const [darkApp, setDarkApp] = useState(false)
+  // const appTheme = darkApp ? MyDarkTheme : MyTheme
   return (
-    // <NavigationContainer theme={appTheme}>
-      <Stack.Navigator initialRouteName={route ? 'Home' : 'FirstScreen'} screenOptions={{ headerShown: false }}>
-        {/* <Stack.Screen name="SplashScreen" component={SplashScreen} /> */}
-        <Stack.Screen name="FirstScreen" component={FirstScreen} />
-        <Stack.Screen name="AddStockScreen" component={AddStockScreen} />
-        <Stack.Screen name="AddStock" component={AddStock} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="SetAlert" component={SetAlert} />
-        <Stack.Screen name="Settings" component={Settings} />
-        <Stack.Screen name="CommunityGroup" component={CommuinityGroup} />
-        <Stack.Screen name="StockScreenBluePrint" component={StockScreen} />
-      </Stack.Navigator>
-    // </NavigationContainer>
+    <Stack.Navigator initialRouteName={route ? 'Home' : 'FirstScreen'} screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="FirstScreen" component={FirstScreen} />
+      <Stack.Screen name="AddStockScreen" component={AddStockScreen} />
+      <Stack.Screen name="AddStock" component={AddStock} />
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="SetAlert" component={SetAlert} />
+      <Stack.Screen name="Settings" component={Settings} />
+      <Stack.Screen name="CommunityGroup" component={CommuinityGroup} />
+      <Stack.Screen name="StockScreenBluePrint" component={StockScreen} />
+    </Stack.Navigator>
   )
 }
 // options={{ headerLeft: null}}
